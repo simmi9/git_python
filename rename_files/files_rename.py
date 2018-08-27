0653b92f643dd7cf456b893fa6eefc94e33d670a
simmi9 <bhawna.keswani20@gmail.com>
2018-08-27 19:54:47 -0400

    Listing files in folder

---
+++ rename_files/files_rename.py
@@ -0,0 +1,10 @@
+import os
+def rename_files():
+    #(1) get file names from a folder
+    file_list = os.listdir(r"C:\Users\ashis\git_python\prank)
+    print(file_list)
+
+    #(2)Foreach file rename file
+
+rename_files ()
+                           
