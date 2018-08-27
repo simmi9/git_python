e3ba8e08ef85b0fbb2c2be90486f37fa717abf50
simmi9 <bhawna.keswani20@gmail.com>
2018-08-27 10:23:36 -0400

    Making program wait longer (2 hours)

--- Take_a_Break/break_time.py
+++ Take_a_Break/break_time.py
@@ -5,7 +5,7 @@ break_count = 0
 print("this program started at" +time.ctime);
 
 while( break_Count < total_breaks):
-  time.sleep(10)
+  time.sleep(2*60*60)
   webbrowser.open("http://www.youtube.com")
   break_count = break_count+1
  
