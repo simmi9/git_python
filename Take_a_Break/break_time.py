42c0be486ed04cf9c12c49f80d62679d59ff473f
simmi9 <bhawna.keswani20@gmail.com>
2018-08-27 10:12:57 -0400

    Added Loop and breaks

--- Take_a_Break/break_time.py
+++ Take_a_Break/break_time.py
@@ -1,4 +1,11 @@
 import time
 import webbrowser
-time.sleep(10)
-webbrowser.open("http://www.youtube.com")
+total_breaks = 3
+break_count = 0
+print("this program started at" +time.ctime);
+
+while( break_Count < total_breaks):
+  time.sleep(10)
+  webbrowser.open("http://www.youtube.com")
+  break_count = break_count+1
+ 
