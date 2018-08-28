03fa0a08f7a53e3c67f3595ea514807c6985223c
simmi9 <bhawna.keswani20@gmail.com>
2018-08-28 12:49:41 -0400

    added function draw_triangle

--- draw_shapes/draw_shapes.py
+++ draw_shapes/draw_shapes.py
@@ -15,15 +15,29 @@ def draw_square():
     
 def draw_circle():
     window1 = turtle.Screen()
+    #window1.bgcolor("yellow")
     angie = turtle.Turtle()
     angie.shape("arrow")
     angie.color("green")
     angie.speed(2)
     angie.circle(100)
-    
-#window1.exitonclick()
+    #window1.exitonclick()
+
+def draw_triangle():
+    window3 = turtle.Screen()
+    #window3.bgcolor("green") #background color
+    tom = turtle.Turtle()
+    tom.color("black")
+    tom.speed(2)
+    tom.forward(100) 
+    tom.left(120)
+    tom.forward(100)
+    tom.left(120)
+    tom.forward(100)
+    window3.exitonclick() 
 
 
 
 draw_square()
 draw_circle()
+draw_triangle()
