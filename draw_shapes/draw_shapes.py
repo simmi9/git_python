48caf68bfe3e70061bd9e1a549b5dc5d8e3af98d
simmi9 <bhawna.keswani20@gmail.com>
2018-08-28 12:30:30 -0400

    fixed bug window and added circle shape to turtle

--- draw_shapes/draw_shapes.py
+++ draw_shapes/draw_shapes.py
@@ -1,6 +1,9 @@
 import turtle
 def draw_square():
+    window = turtle.Screen()
+    
     brad = turtle.Turtle()
+
     brad.shape( "turtle")
     brad.color("red")
     brad.speed(2)
@@ -12,6 +15,12 @@ def draw_square():
     brad.right(90)
     brad.forward(100)
 
+    angie = turtle.Turtle()
+    angie.shape("arrow")
+    angie.color("green")
+    angie.speed(2)
+    angie.circle(100)
+    
     window.exitonclick()
 
 
