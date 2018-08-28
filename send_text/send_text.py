7bfe97e3ebb26531c6017a0f58d9fe3000b75a16
simmi9 <bhawna.keswani20@gmail.com>
2018-08-28 15:56:50 -0400

    sensiflow

--- send_text/send_text.py
+++ send_text/send_text.py
@@ -3,12 +3,12 @@ import twilio.rest
 from twilio.rest import Client
 #your account sid and auth token from twilio.com/user/account
 
-account_sid= "ACa56edcc24ad71ec70d155f0fb016171f"
-auth_token = "eb8b28681edb4ca63519c2b9296905fc"
+account_sid= "ACa56edcc24ad71ec70d155f*********"
+auth_token = "eb8b28681edb4ca***************"
 client = Client(account_sid , auth_token)
 
 message = client.messages.create(
     body = "My name is Bhawna",
-    to = "+16473282741", #twilio phone number
+    to = "+16*********", #twilio phone number
     from_ = "3068024328 " ) #your phone number
 print (message.sid)
