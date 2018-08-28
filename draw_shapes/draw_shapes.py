67091972e2f71fde26ec2c72114a453aeacdd797
simmi9 <bhawna.keswani20@gmail.com>
2018-08-28 12:42:01 -0400

    made seperate functions for draw_square , draw_circle

--- draw_shapes/draw_shapes.py
+++ draw_shapes/draw_shapes.py
@@ -1,28 +1,29 @@
 import turtle
 def draw_square():
     window = turtle.Screen()
-    
     brad = turtle.Turtle()
-
     brad.shape( "turtle")
     brad.color("red")
     brad.speed(2)
-    brad.forward(100)
-    brad.right(90)
-    brad.forward(100)
-    brad.right(90)
-    brad.forward(100)
-    brad.right(90)
-    brad.forward(100)
+    number = 0
 
+    while (number != 4):
+        brad.forward(100)
+        brad.right(90)
+        number = number + 1
+
+    
+def draw_circle():
+    window1 = turtle.Screen()
     angie = turtle.Turtle()
     angie.shape("arrow")
     angie.color("green")
     angie.speed(2)
     angie.circle(100)
     
-    window.exitonclick()
+#window1.exitonclick()
 
 
 
 draw_square()
+draw_circle()
