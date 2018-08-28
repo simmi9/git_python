601a640e756c9a10c92e21868a815891825b00b0
simmi9 <bhawna.keswani20@gmail.com>
2018-08-28 12:13:18 -0400

    Added new folder albhabet and created draw_shapes function for square

---
+++ draw_shapes/draw_shapes.py
@@ -0,0 +1,16 @@
+import turtle
+def draw_square():
+    brad = turtle.Turtle()
+    brad.forward(100)
+    brad.right(90)
+    brad.forward(100)
+    brad.right(90)
+    brad.forward(100)
+    brad.right(90)
+    brad.forward(100)
+
+    window.exitonclick()
+
+
+
+draw_square()
--- rename_files/files_rename.py
+++ rename_files/files_rename.py
@@ -6,10 +6,14 @@ def rename_files():
     print(file_list)
 
     #(2)Foreach file rename file
-    translation = {0:None,1:None, 2:None,3:None,4:None,5:None,6:None,7:None,8:None,9:None}
-    
+    os.chdir(r"C:\Users\ashis\git_python\prank")
+    #translation = {"0":None,"1":None, "2":None,"3":None,"4":None,"5":None,"6":None,"7":None,"8":None,"9":None}
+    translation={"0123456789" : None }
     for file_name in file_list: os.rename(file_name, file_name.translate(translation))
-                                          
+                                   
 
 rename_files ()
+
+os.listdir(r"C:\Users\ashis\git_python\prank")
+
                            
