31fc8a2acaf24340034bdc10dfe70c125a99ee47
simmi9 <bhawna.keswani20@gmail.com>
2018-08-27 20:40:53 -0400

    Renamed files using translate

--- rename_files/files_rename.py
+++ rename_files/files_rename.py
@@ -1,10 +1,15 @@
 import os
+
 def rename_files():
     #(1) get file names from a folder
-    file_list = os.listdir(r"C:\Users\ashis\git_python\prank)
+    file_list = os.listdir(r"C:\Users\ashis\git_python\prank")
     print(file_list)
 
     #(2)Foreach file rename file
+    translation = {0:None,1:None, 2:None,3:None,4:None,5:None,6:None,7:None,8:None,9:None}
+    
+    for file_name in file_list: os.rename(file_name, file_name.translate(translation))
+                                          
 
 rename_files ()
                            
