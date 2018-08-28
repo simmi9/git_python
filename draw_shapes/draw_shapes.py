b451aedf0b61f9cc684b7a5a7beb290a2cf73ab0
simmi9 <bhawna.keswani20@gmail.com>
2018-08-28 12:17:47 -0400

    changed shape, color, speed of turtle

--- draw_shapes/draw_shapes.py
+++ draw_shapes/draw_shapes.py
@@ -1,6 +1,9 @@
 import turtle
 def draw_square():
     brad = turtle.Turtle()
+    brad.shape( "turtle")
+    brad.color("white")
+    brad.speed(2)
     brad.forward(100)
     brad.right(90)
     brad.forward(100)
